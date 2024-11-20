f6e52829c0cd838de8cf4ff649311465a0af764a
Shristirajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2024-11-20 11:02:30 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { authMiddleware } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/me', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
